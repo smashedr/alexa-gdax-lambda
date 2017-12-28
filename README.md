92c432c0f245d3645f425e57e1d66498e679afd0
Shane <shane@shanerice.me>
2017-12-27 21:14:35 -0800

    Adding to GitLab

---
+++ README.md
@@ -0,0 +1,31 @@
+# Alexa GDAX
+
+[![build status](https://git.cssnr.com/shane/alexa-gdax/badges/master/build.svg)](https://git.cssnr.com/shane/alexa-gdax/commits/master) [![coverage report](https://git.cssnr.com/shane/alexa-gdax/badges/master/coverage.svg)](https://git.cssnr.com/shane/alexa-gdax/commits/master)
+
+This skill lets you look GDAX.com account information with Alexa.
+
+## Phrases
+
+- ...
+- ...
+- ...
+
+# Skill Information
+
+Information used when submitting the Skill.
+
+## Testing Instructions
+
+...
+
+## Short Skill Description
+
+...
+
+## Full Skill Description
+
+...
+
+## Keywords
+
+...
---
+++ lambda_function.py
@@ -0,0 +1,72 @@
+import requests
+
+TXT_UNKNOWN = 'I did not understand that request, please try something else.'
+TXT_ERROR = 'Error looking up {}, please try something else.'
+
+KEYS = {
+    'bitcoin': 'BTC',
+    'bitcoin cash': 'BCH',
+    'litecoin': 'LTC',
+    'ethereum': 'ETH',
+}
+
+
+def build_speech_response(title, output, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': "SessionSpeechlet - " + title,
+            'content': "SessionSpeechlet - " + output
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+
+def alexa_response(session_attributes, speech_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speech_response
+    }
+
+
+def alexa_error(error='Unknown error, please try something else', title='UE'):
+    alexa = alexa_response(
+        {}, build_speech_response(title, error, None, True)
+    )
+    return alexa
+
+
+def acct_overview(event):
+    alexa = alexa_response(
+        {},
+        build_speech_response(
+            'Accounts Overview', 'Coming soon...', None, True
+        )
+    )
+    return alexa
+
+
+def lambda_handler(event, context):
+    print('event: {}'.format(event))
+    try:
+        intent = event['request']['intent']['name']
+        if intent == 'AccountOverview':
+            return acct_overview(event)
+        else:
+            raise ValueError('Unknown Intent')
+    except ValueError:
+        return alexa_error()
+    except Exception as error:
+        print('error: {}'.format(error))
+        return alexa_error()
